c22037e3e80fdeb5a6a300a6242818eefe446483
JeffKirigaya <jeffersonlucas123321@gmail.com>
2026-02-17 03:35:45 -0300

    Meu exemplo e aula 12 ex

---
+++ Aula12ex/script2.js
@@ -0,0 +1,43 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('age')
+    var res = document.getElementById('res')
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >= 0 && idade < 18) {
+                //criança
+                img.setAttribute('src', 'imagens/menino.png')
+            } else if (idade < 50) {
+                //adulto
+                img.setAttribute('src', 'imagens/adulto.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'imagens/velho.png')
+            }
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade >= 0 && idade < 18) {
+                //criança
+                img.setAttribute('src', 'imagens/menina.png')
+            } else if (idade < 50) {
+                //adulto
+                img.setAttribute('src', 'imagens/adulta.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'imagens/velha.png')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
+    }
+}
